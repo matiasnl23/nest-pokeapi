6b02ee1fce138a0054839cea6ac37fc95de635ce
Matías Lobo <matiasnl.1993@gmail.com>
2024-01-21 01:32:04 -0300

    Se termina con el SEED de pokemons

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { Model } from 'mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
+  constructor(
+    @InjectModel(Pokemon.name) readonly pokemonModel: Model<Pokemon>,
+  ) { }
+
   async executeSeed() {
+    await this.pokemonModel.deleteMany({});
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=10',
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
 
     const pokemons = data.results.map(({ name, url }) => {
@@ -17,6 +25,8 @@ export class SeedService {
       return { name, no };
     });
 
-    return pokemons;
+    await this.pokemonModel.insertMany(pokemons);
+
+    return 'Seed executed';
   }
 }
