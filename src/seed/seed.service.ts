d3591d6499e6f8b048abd8ce71716e7f67fbe969
Matías Lobo <matiasnl.1993@gmail.com>
2024-01-21 01:13:55 -0300

    SEED Pokemons (Parte 1)

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  executeSeed() {
-    return 'Seed executed';
+  private readonly axios: AxiosInstance = axios;
+
+  async executeSeed() {
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=10',
+    );
+
+    const pokemons = data.results.map(({ name, url }) => {
+      const no = +url.split('/').slice(-2, -1)[0];
+
+      return { name, no };
+    });
+
+    return pokemons;
   }
 }
